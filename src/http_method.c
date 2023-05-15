0f7c2a83756fd27bd90c9211a66482cdf1953f16
Lrmmm <ubuntu@localhost.localdomain>
2023-05-15 16:38:26 +0800

    解析url

--- src/http_method.c
+++ src/http_method.c
@@ -47,6 +47,7 @@ void do_http_request(int client_sock)
     char buf[256];
     char method[64];
     char url[256];
+    char path[256];
 
     // 读取客户端发送的http请求
 
@@ -65,15 +66,19 @@ void do_http_request(int client_sock)
         }
         method[i] = '\0';
 #ifdef DEBUG
+        printf("buf: %s\n",buf);
         printf("request method: %s\n", method);
 #endif
 
-        if (strncasecmp(method , "GET", sizeof("GET")) == 0){
+        if (strncasecmp(method , "GET", sizeof("GET")) == 0){  // 处理GET请求
 #ifdef DEBUG
             printf("method = GET\n");
 #endif
             // 获取url
-            while (isspace(buf[j++]));
+            while (isspace(buf[j]))
+            {
+                j++;
+            };
             i = 0;
             
             while (!isspace(buf[j]) && (i < sizeof(url) - 1))
@@ -87,18 +92,48 @@ void do_http_request(int client_sock)
 
 #ifdef DEBUG
             printf("url: %s\n", url);
-#endif          
+#endif 
+            // 2.读取http头部
+            buf_len = 0;
+            memset(buf, 0, sizeof(buf));
+            do {
+                buf_len = get_line(client_sock, buf, sizeof(buf));
+#ifdef DEBUG
+                printf("read: %s\n", buf);
+#endif
+            }while(buf_len > 0); 
+
+
+            // 3.定位服务器的html文件
+            {
+                char* pos = strchr(url, '?');
+                if(pos) {
+                    *pos = '\0';
+#ifdef DEBUG
+                    printf("real url: %s\n", url);
+#endif
+                }
+                sprintf(path, "../docs%s", url);
+#ifdef DEBUG
+                printf("path: %s\n",path);
+#endif
+
+            }
+        }
+        else  // 处理非GET请求,读取http 头部，并响应客户端501  Method Not Implemented
+        {
+            fprintf(stderr, "warning! other request [%s]\n", method);
+            buf_len = 0;
+            memset(buf, 0, sizeof(buf));
+            do {
+                buf_len = get_line(client_sock, buf, sizeof(buf));
+#ifdef DEBUG
+                printf("read: %s\n", buf);
+#endif
+            }while(buf_len > 0); 
+
         }
     }
 
-    // 2.读取http头部
-    buf_len = 0;
-    memset(buf, 0, sizeof(buf));
 
-    do {
-        buf_len = get_line(client_sock, buf, sizeof(buf));
-#ifdef DEBUG
-        printf("read: %s\n", buf);
-#endif
-    }while(buf_len > 0);
 }
